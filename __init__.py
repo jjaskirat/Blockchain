0053ff5d9219cb057da0ec1ad98c1e64a2ca1f8e
jjaskirat <jaskiratsinghbhatia4@gmail.com>
2020-03-28 13:12:42 -0400

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from rabadiom.blockchain.utils import BlockChain, Patient, Doctor, Transaction, EHR
+from rabadiom.models import Blockchain, User, Keys, Ehr
+#from flaskblog import app
+
+from rabadiom.blockchain.utils import BlockChain
+blockchain = BlockChain()
